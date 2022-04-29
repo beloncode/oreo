260332ee5430755b70c89280fefce486a0466a55
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-04-28 23:42:08 -0400

    Improving code quality

--- arch/asm_syscall.h
+++ arch/asm_syscall.h
@@ -6,11 +6,6 @@
 #ifndef ARCH_ASM_SYSCALL_H
 #define ARCH_ASM_SYSCALL_H
 
-/* This code is part of OREO project, all rights reserved to the owners
- * Wrote by: [Gabriel Correia]
- * Create at: 2022/04/28
-*/
-
 #include "libc/stddef.h"
 
 static __inline u64_t
@@ -28,10 +23,10 @@ static __inline u64_t
 __syscall3
 (u64_t num, u64_t rdi, u64_t rsi, u64_t rdx)
 {
-	u64_t ret;
-	__asm__ __volatile__("syscall" : "=a"(ret) : "a"(num), "D"(rdi), "S"(rsi),
-	  "d"(rdx) : "rcx", "r11", "memory");
-	return (ret);
+  u64_t ret;
+  __asm__ __volatile__("syscall" : "=a"(ret) : "a"(num), "D"(rdi), "S"(rsi),
+    "d"(rdx) : "rcx", "r11", "memory");
+  return (ret);
 }
 
 #endif
